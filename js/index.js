fc8195c7e489aa36a200e262178c9b5d62cb91e5
Emelie Barrington <emelie.barrington@chasacademy.se>
2022-12-04 17:34:19 +0100

    Did some cleanup and validation tests

--- js/index.js
+++ js/index.js
@@ -1,30 +1,15 @@
-let noteArea = document.getElementById("txtContent");
+
+//Denna variabel används aldrig
+//let noteArea = document.getElementById("txtContent");
 let editButton = document.getElementById("edit-icon");
-let navlinks = document.querySelectorAll(".navlinks");
+//Denna variabel används aldrig
+//let navlinks = document.querySelectorAll(".navlinks");
 
 editButton.addEventListener('click', edit);
-
-
-
-/*   for (let i = 0; i < navlinks.length; i++) {
-     navlinks[i].addEventListener("click", readonly);
- } */
-
 function edit() {
     document.getElementById("txtContent").setAttribute("contenteditable", "true");
-    /* console.log("Edit ran"); */
 }
 
-/* function readonly () {
-    document.getElementById("txtContent").setAttribute("readonly", "readonly");
-    
-} */
-
-
-// localStorage.setItem("test", true, 0); 
-// const testing = localStorage.getItem("test");
-
-// console.log(testing); 
 
 const modal = document.querySelector(".modal"); 
 let pageVisited = localStorage.getItem("modalShown"); 
@@ -46,49 +31,41 @@ else {
 // Funktioner för att spara titel/text
 
 function saveTitle() {
-    //Make sure local storage is supported by the browser.
+//Make sure local storage is supported by the browser.
     if (typeof(Storage) !== 'undefined') {
-      //Get title value
-      const input = document.getElementById('txtHeader').value;
-    
-
-      
-      //Save title to local Storage
-      localStorage.setItem('txtHeader', input);
-      document.getElementById('txtHeader').value = localStorage.getItem('txtHeader');
-       console.log(input)
-      //Test
-        console.log('Title saved.')
-        }
+        //Get title value
+        const input = document.getElementById('txtHeader').value;
+        //Save title to local Storage
+        localStorage.setItem('txtHeader', input);
+        document.getElementById('txtHeader').value = localStorage.getItem('txtHeader');
+        console.log(input)
+        //Test
+        console.log('Title saved.');
     }
+}
+document.getElementById('txtHeader').value = localStorage.getItem('txtHeader')
 
-    document.getElementById('txtHeader').value = localStorage.getItem('txtHeader')
-
-      //Save note message
-      function saveMessage() {
-        if (typeof(Storage) != 'undefined') {
-        //Get value of the message
-        const messageInput = document.getElementById('txtContent').innerHTML;
-        //Save the value in local storage.
-        localStorage.setItem('txtContent', messageInput);
-        document.getElementById('txtContent').innerHTML = localStorage.getItem('txtContent')
-        }
-        console.log("saveMessage ran")
-  
-  }
-  
-  //Get the value of the message from local storage
-  document.getElementById('txtContent').innerHTML = localStorage.getItem('txtContent')
+//Save note message
+function saveMessage() {
+    if (typeof(Storage) != 'undefined') {
+    //Get value of the message
+    const messageInput = document.getElementById('txtContent').innerHTML;
+    //Save the value in local storage.
+    localStorage.setItem('txtContent', messageInput);
+    document.getElementById('txtContent').innerHTML = localStorage.getItem('txtContent');
+    }
+    //Test
+    console.log("saveMessage ran"); 
+}
+//Get the value of the message from local storage
+document.getElementById('txtContent').innerHTML = localStorage.getItem('txtContent')
 
 const placeholderButton = document.getElementById("placeholder-button");
-
 placeholderButton.addEventListener("click", () => {    
-    saveMessage();
-    saveTitle();    
+    //Skapa ny anteckning och spara den gamla till menyn på sidan 
 });
 
 const buttons = document.querySelectorAll(".btn"); 
-
 buttons.forEach(button => {
     button.addEventListener("click", () => {
         let command = button.dataset["element"];
