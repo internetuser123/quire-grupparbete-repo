7f6a6886140cb63adc707057a41b9c1cd236b329
Joel Sellgren <joel.sellgren@chasacademy.se>
2022-12-06 17:12:59 +0100

    functions for add note to sidemenu
    Co-authored-by: Emelie Barrington <barringtonemelie@users.noreply.github.com>

--- js/index.js
+++ js/index.js
@@ -1,34 +1,30 @@
 let noteArea = document.getElementById("txtContent");
 let editButton = document.getElementById("edit-icon");
-let navlinks = document.querySelectorAll(".navlinks");
-const noteList = document.querySelector(".noteList");
-const addNoteButton = noteList.querySelector(".add-note");
+/* let navlinks = document.querySelectorAll(".navlinks"); */
+const noteList = document.getElementById("note-list");
+const addNoteButton = document.querySelector(".add-note");
+const noteUL = document.querySelector(".note-ul");
 
 
+// Knapp för att redigera
 editButton.addEventListener('click', edit);
-
-
-
-/*   for (let i = 0; i < navlinks.length; i++) {
-     navlinks[i].addEventListener("click", readonly);
- } */
-
 function edit() {
     document.getElementById("txtContent").setAttribute("contenteditable", "true");
-    /* console.log("Edit ran"); */
+
 }
 
-/* function readonly () {
-    document.getElementById("txtContent").setAttribute("readonly", "readonly");
-    
-} */
+// Knappar för att styla texten
+const buttons = document.querySelectorAll(".btn"); 
+
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+        let command = button.dataset["element"];
+        document.execCommand(command, false, null); 
+    }); 
+}); 
 
 
-// localStorage.setItem("test", true, 0); 
-// const testing = localStorage.getItem("test");
-
-// console.log(testing); 
-
+// Modal
 const modal = document.querySelector(".modal"); 
 let pageVisited = localStorage.getItem("modalShown"); 
 
@@ -47,21 +43,16 @@ else {
 }
 
 // Funktioner för att spara titel/text
-
 function saveTitle() {
     //Make sure local storage is supported by the browser.
     if (typeof(Storage) !== 'undefined') {
       //Get title value
       const input = document.getElementById('txtHeader').value;
     
-
-      
       //Save title to local Storage
       localStorage.setItem('txtHeader', input);
       document.getElementById('txtHeader').value = localStorage.getItem('txtHeader');
        console.log(input)
-      //Test
-        console.log('Title saved.')
         }
     }
 
@@ -77,71 +68,104 @@ function saveTitle() {
         document.getElementById('txtContent').innerHTML = localStorage.getItem('txtContent')
         }
         console.log("saveMessage ran")
-  
   }
   
   //Get the value of the message from local storage
   document.getElementById('txtContent').innerHTML = localStorage.getItem('txtContent')
 
-const placeholderButton = document.getElementById("placeholder-button");
+const saveButton = document.getElementById("save-button");
 
-placeholderButton.addEventListener("click", () => {    
+saveButton.addEventListener("click", () => {    
     saveMessage();
-    saveTitle();    
+    saveTitle();  
+    addToNoteList();
 });
 
 
-const buttons = document.querySelectorAll(".btn"); 
-
-buttons.forEach(button => {
-    button.addEventListener("click", () => {
-        let command = button.dataset["element"];
-        document.execCommand(command, false, null); 
-    }); 
-}); 
 
 
+function addToNoteList() {
+  const title = document.getElementById('txtHeader').value;
+  const content = document.getElementById('txtContent').innerHTML;
+  const currentNote = {
+    title: title,
+    content: content,
+    id: Math.floor(Math.random() * 100000)
+  }
+  addNote(currentNote);
+  console.log(currentNote.id);
+}
+
+
+/* function getActiveNote()  {
+  let activeNote = document.getElementById(currentNote.id);
+  //TODO: Change styling to display which note is active 
+}
+activeNote.addEventListener("click", getActiveNote); */
 
 
 
 
 
+// Funktioner för att lägga till/spara notes
 getNotes().forEach((note) => {
-    const noteElement = createNoteElement(note.id, note.content);
-    notesContainer.insertBefore(noteElement, addNoteButton);
+  const noteElement = createNoteElement(note.id, note.title);
+  noteUL.appendChild(noteElement)
+  /* noteList.insertBefore(noteElement, addNoteButton); */
+});
+
+/* addNoteButton.addEventListener("click", () => addNote()); */
+
+function getNotes() {
+  return JSON.parse(localStorage.getItem("notes") || "[]");
+}
+
+function saveNotes(notes) {
+  localStorage.setItem("notes", JSON.stringify(notes));
+}
+
+
+
+
+
+function createNoteElement(id, content) {
+  const element = document.createElement("li");
+  /* noteUL.appendChild(element) */
+  console.log(noteUL);
+
+  element.classList.add("note");
+  element.innerHTML = content;
+  element.setAttribute("id", id)
+
+  element.addEventListener("change", () => {
+    updateNote(id, element.value);
   });
 
-  addNoteButton.addEventListener("click", () => addNote());
+  return element;
 
-  function getNotes() {
-    return JSON.parse(localStorage.getItem("stickynotes-notes") || "[]");
-  }
-
-  function createNoteElement(id, content) {
-    const element = document.createElement("div");
-    element.setAttribute("contenteditable", "true");
-  
-    element.classList.add("note");
-    element.value = content;
-    element.placeholder = "Empty Sticky Note";
-  
-    element.addEventListener("change", () => {
-      updateNote(id, element.value);
+}
 
 
-    })
 
-    return element;
+function addNote(noteObject) {
+  const notes = getNotes();
 
-};
+  const noteElement = createNoteElement(noteObject.id, noteObject.title);
+  noteUL.appendChild(noteElement)
+  /* noteList.insertBefore(noteElement, addNoteButton);  *///Ändra notelist till ULen
 
-function addNote() {
-    const notes = getNotes();
-    const noteObject = {
-      id: Math.floor(Math.random() * 100000),
-      content: ""
-      
-    }
-    console.log("addnote ran")
-};
+  notes.push(noteObject);
+  saveNotes(notes);
+  console.log("noteobject", noteObject);
+}
+
+
+
+function updateNote(id, newContent) {
+  const notes = getNotes();
+  const targetNote = notes.filter((note) => note.id == id)[0];
+
+  targetNote.content = newContent;
+  saveNotes(notes);
+}
 
